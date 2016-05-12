31a3c7c5a14aa292af966778d9ed6438305a3eff
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2016-05-12 10:29:20 -0300

    cambios

--- Galaga/src/Modelo/MotorJuego.java
+++ Galaga/src/Modelo/MotorJuego.java
@@ -5,39 +5,48 @@ import java.util.LinkedList;
 public class MotorJuego {
 	private Espacio espacio;
 	private Jugador naveJugador;
-	private LinkedList<Nave> navesEnemigas;
-	private LinkedList<Municion> municiones;
+	private LinkedList<ObjetoMovil> objetosMoviles;
+//	private LinkedList<Municion> municiones;
 	
 	public MotorJuego() {
 		this.espacio = new Espacio(512, 512);
 //		this.naveJugador = new Jugador(x, y, vidas);
-		this.navesEnemigas = new LinkedList<>();
-		this.municiones = new LinkedList<>();
+		this.objetosMoviles = new LinkedList<>();
+//		this.municiones = new LinkedList<>();
 	}
 	
-	public void agregarNaveEnemiga(Nave naveEnemiga) {
-		this.navesEnemigas.add(naveEnemiga);
+	public void agregarObjetoMovil(Nave naveEnemiga) {
+		this.objetosMoviles.add(naveEnemiga);
 	}
 	
-	public Nave getNaveEnemiga(int posicion){
-		return this.navesEnemigas.get(posicion);
-	}
+//	public Nave getNaveEnemiga(int posicion){
+//		return this.objetosMoviles.
+//	}
 	
 	public Nave getNaveJugador() {
 		return naveJugador;
 	}
 	
-	public void agregarMuniciones(Municion municion) {
-		// TODO Auto-generated method stub
-		this.municiones.add(municion);
-
-	}
-	
-	public Municion getMuniciones(int posicion) {
-		return this.municiones.get(posicion);
-	}
+//	public void agregarMuniciones(Municion municion) {
+//		// TODO Auto-generated method stub
+//		this.municiones.add(municion);
+//
+//	}
+//	
+//	public Municion getMuniciones(int posicion) {
+//		return this.municiones.get(posicion);
+//	}
 	
 	public void jugar() {
+		
+		
+		for (int i = 0; i < 10; i++) {
+			
+			for (int j = 0; j < this.objetosMoviles.size(); j++) {
+				this.objetosMoviles.get(j).mover();
+			}
+		}
+		
 		
 		
 	}
---
+++ Galaga/src/Modelo/Probar.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public class Probar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
